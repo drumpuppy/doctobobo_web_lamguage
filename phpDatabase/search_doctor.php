359192ba8c3a39843b840b415646c8f38ca43478
etienne lazarz <e.lazarz@mytower.co>
2023-05-18 16:12:48 +0200

    revised search to include single arg search

--- phpDatabase/search_doctor.php
+++ phpDatabase/search_doctor.php
@@ -11,34 +11,47 @@ function searchDoctors($name, $specialty, $location) {
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    $sql = "SELECT * FROM Medecin WHERE Nom_Medecin LIKE ? OR Specialite LIKE ? OR code_postal LIKE ?";
 
+    $sql = "SELECT * FROM Medecin WHERE 1=1";
+    $types = '';
+    $params = [];
+
+
+    if (!empty($name)) {
+        $sql .= " AND Nom_Medecin LIKE ?";
+        $types .= 's';
+        $params[] = &$name;
+    }
+    if (!empty($specialty)) {
+        $sql .= " AND Specialite LIKE ?";
+        $types .= 's';
+        $params[] = &$specialty;
+    }
+    if (!empty($location)) {
+        $sql .= " AND code_postal LIKE ?";
+        $types .= 's';
+        $params[] = &$location;
+    }
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("sss", $name, $specialty, $location);
-
+    if ($types) {
+        $stmt->bind_param($types, ...$params);
+    }
     $stmt->execute();
-
     $result = $stmt->get_result();
-
     $doctors = $result->fetch_all(MYSQLI_ASSOC);
     $stmt->close();
     $conn->close();
-
     return $doctors;
 }
 
-$name = "%" . $_POST['nom'] . "%";
-$specialty = "%" . $_POST['specialite'] . "%";
-$location = "%" . $_POST['lieu'] . "%";
+$name = isset($_POST['nom']) ? "%" . $_POST['nom'] . "%" : '';
+$specialty = isset($_POST['specialite']) ? "%" . $_POST['specialite'] . "%" : '';
+$location = isset($_POST['lieu']) ? "%" . $_POST['lieu'] . "%" : '';
 
 $doctors = searchDoctors($name, $specialty, $location);
 
-// Save the results to a session variable
 session_start();
 $_SESSION['search_results'] = $doctors;
-
-// Redirect to the result page
 header("Location: ../result.html");
 exit();
-
 ?>
